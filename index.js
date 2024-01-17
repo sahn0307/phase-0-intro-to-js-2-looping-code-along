f746ab7d3b5ab8df6f09c5bd85ef5582c42eb308
Sung-Jin Ahn <sahn0307@gmail.com>
2024-01-16 17:05:24 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  function countDown(startingNumber) {
+    // Check if the input is a positive integer
+    if (startingNumber < 0 || !Number.isInteger(startingNumber)) {
+      console.log("Please provide a positive integer.");
+      return;
+    }
+  
+    for (let i = startingNumber; i >= 0; i--) {
+      console.log(i);
+    }
+  }
